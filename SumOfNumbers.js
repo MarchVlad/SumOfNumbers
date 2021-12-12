472772e680726d638f5b8e31d89651afe48f06b2
MarchVlad <ktotogdeto849@gmail.com>
2021-12-12 20:08:47 +0200

    Repair mistakes

--- SumOfNumbers.js
+++ SumOfNumbers.js
@@ -1,3 +1,11 @@
 let num1 = process.argv[2], num2 = process.argv[3];
 
-console.log(`Result: ${Number(num1) + Number(num2)}`);
+if(Number.isInteger(Number(num1)) && Number.isInteger(Number(num2)))
+{
+    console.log(`Result: ${Number(num1) + Number(num2)}`);
+    
+}
+else
+{
+    console.log("It's not a number!");
+}
